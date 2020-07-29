9df03c152bd03a72504259a76d0b4f47e211e738
Gabriel Hideki Miyoshi <gabriel_hidekii@hotmail.com>
2020-07-28 22:39:44 -0300

    Finalizando page 404

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import PageDefault from '../../components/PageDefault';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
     <PageDefault>
       <h1>Página não encontrada!</h1>
 
-      <img src="https://64.media.tumblr.com/tumblr_lmjwp55r9h1qg0puro1_500.gif"></img>
+      <img src="https://64.media.tumblr.com/tumblr_lmjwp55r9h1qg0puro1_500.gif" style={{ width: '100%' }}></img>
     </PageDefault>
   )
 }
